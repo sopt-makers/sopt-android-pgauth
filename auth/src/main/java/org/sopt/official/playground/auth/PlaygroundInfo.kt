4babe47d2092252a80284f071f32568757d321ab
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-04-19 23:16:25 +0900

    dev api host 변경

--- auth/src/main/java/org/sopt/official/playground/auth/PlaygroundInfo.kt
+++ auth/src/main/java/org/sopt/official/playground/auth/PlaygroundInfo.kt
@@ -5,6 +5,6 @@ internal object PlaygroundInfo {
     val HOST: String = "playground.sopt.org"
     val AUTH_PATH: String = "auth/oauth"
     val REDIRECT_URI = "app://org.sopt.makers.android/oauth2redirect"
-    val DEBUG_AUTH_HOST: String = "ec2-43-200-170-159.ap-northeast-2.compute.amazonaws.com:8080/api/v2/"
+    val DEBUG_AUTH_HOST: String = "app.dev.sopt.org/api/v2/"
     val RELEASE_AUTH_HOST: String = "app.sopt.org/api/v2/"
 }
